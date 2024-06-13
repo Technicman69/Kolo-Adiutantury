3d589d5c4c8badd6d39b4be417ef4fdf7164f9fe
rpyda <rpyda@mion>
2024-06-13 17:43:45 +0200

    Wczytywanie z plików

---
+++ src/main/java/Utils.java
@@ -0,0 +1,38 @@
+package main.java;
+import java.awt.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
+public class Utils {
+    public static ArrayList<Student> wczytajPlik(String path)
+    {
+        try {
+            int i = 0;
+            Color colors[] = {Color.GREEN, Color.YELLOW, Color.BLUE, Color.RED};
+            Scanner odczyt = new Scanner(new File(path));
+            ArrayList<Student> Lista = new ArrayList<Student>();
+            while(odczyt.hasNextLine()) {
+                String line = odczyt.nextLine();
+                String[] nazwy = line.split(" ");
+                String fullname = (nazwy[0] + " " + nazwy[1]);
+                double weight = Double.parseDouble(nazwy[2]);
+                Color color = colors[i%4];
+                i++;
+                Lista.add(new Student(fullname, weight, color));
+
+            }
+            return Lista;
+        }
+        catch (Exception e)
+        {
+            System.out.println("error");
+            return new ArrayList<Student>();
+        }
+
+    }
+    public static void main(String[] args) {
+        Utils.wczytajPlik("aaa.txt");
+    }
+}
